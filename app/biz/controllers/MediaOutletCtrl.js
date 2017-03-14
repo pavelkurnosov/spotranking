7a22485db83cee3b3c799c8c4ae2703e66af5ce4
pavelkurnosov <pavel.kurnosov2016@yandex.com>
2017-03-14 17:56:10 +0100

    adding media logo in media outlet page

--- app/biz/controllers/MediaOutletCtrl.js
+++ app/biz/controllers/MediaOutletCtrl.js
@@ -2,6 +2,13 @@
 
 angular.module('app.biz').controller('MediaOutletController', function (DTOptionsBuilder, DTColumnBuilder) {
     var vm = this;
+
+    vm.medias = [
+        { title: "Print Media", image: "./styles/img/temp/1.jpg"},
+        { title: "TV Channel", image: "./styles/img/temp/2.jpg"},
+        { title: "FM Radio", image: "./styles/img/temp/3.jpg"}
+    ];
+
     vm.standardOptions = DTOptionsBuilder
         .fromSource('app/biz/outlet_data.json')
         .withDOM("<'dt-toolbar'<'col-xs-12 col-sm-6'f><'col-sm-6 col-xs-12 hidden-xs'l>r>t<'dt-toolbar-footer'<'col-sm-6 col-xs-12 hidden-xs'i><'col-xs-12 col-sm-6'p>>")
