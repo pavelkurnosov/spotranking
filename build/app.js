07aa1847ac0ed967cbe34a73b9e5bc2540bb6f28
pavelkurnosov <pavel.kurnosov2016@yandex.com>
2017-03-18 02:06:39 +0100

    done youtube page

--- build/app.js
+++ build/app.js
@@ -5820,7 +5820,7 @@ angular.module('app.biz').controller('YoutubeController', function () {
         yAxis: {reversedStacks: false, visible: false, gridLineColor: 'transparent', min: 0, title: {text: ''}},
         legend: {reversed: false},
         plotOptions: {series: {stacking: 'normal'}},
-        colors: ['#61c2f6'],
+        colors: ['#e34748'],
         series: [{
             showInLegend: false,
             name: 'Twitter',
@@ -5836,7 +5836,7 @@ angular.module('app.biz').controller('YoutubeController', function () {
         yAxis: {reversedStacks: false, visible: false, gridLineColor: 'transparent', min: 0, title: {text: ''}},
         legend: {reversed: false},
         plotOptions: {series: {stacking: 'normal'}},
-        colors: ['#61c2f6'],
+        colors: ['#e34748'],
         series: [{
             showInLegend: false,
             name: 'Twitter',
@@ -5852,13 +5852,11 @@ angular.module('app.biz').controller('YoutubeController', function () {
         yAxis: {reversedStacks: false, visible: false, gridLineColor: 'transparent', min: 0, title: {text: ''}},
         legend: {reversed: false},
         plotOptions: {series: {stacking: 'normal'}},
-        colors: ['#65c1ee', '#294a83'],
+        colors: ['#6b7889'],
         series: [{
+            showInLegend: false,
             name: 'Retweets',
-            data: [130, 100, 52, 35, 26, 18, 10, 8, 4, 3, 3, 1]
-        }, {
-            name: 'Favorites',
-            data: [40, 12, 8, 6, 9, 2, 1, 2, 1, 1, 2, 2]
+            data: [130, 100, 52, 35, 26, {y:18, color: '#64b0ec'}, 10, 8, 4, 3, 3, 1]
         }]
     };
     vm.data5 = {
@@ -5870,11 +5868,11 @@ angular.module('app.biz').controller('YoutubeController', function () {
         yAxis: {reversedStacks: false, visible: false, gridLineColor: 'transparent', min: 0, title: {text: ''}},
         legend: {reversed: false},
         plotOptions: {series: {stacking: 'normal'}},
-        colors: ['#294a83'],
+        colors: ['#6b7889'],
         series: [{
             showInLegend: false,
             name: 'Retweets',
-            data: [130, 100, 52, 35, 26, 18, 10, 8, 4, 3, 3, 1]
+            data: [130, 100, 52, 35, 26, {y:18, color: '#64b0ec'}, 10, 8, 4, 3, 3, 1]
         }]
     };
     vm.data6 = {
@@ -5886,16 +5884,86 @@ angular.module('app.biz').controller('YoutubeController', function () {
         yAxis: {reversedStacks: false, visible: false, gridLineColor: 'transparent', min: 0, title: {text: ''}},
         legend: {reversed: false},
         plotOptions: {series: {stacking: 'normal'}},
-        colors: ['#65c1ee', '#294a83'],
+        colors: ['#6b7889'],
         series: [{
-            name: 'Tweets',
-            data: [130, 100, 52, 35, 26, 18, 10, 8, 4, 3, 3, 1]
-        }, {
+            showInLegend: false,
             name: 'Retweets',
-            data: [40, 12, 8, 6, 9, 2, 1, 2, 1, 1, 2, 2]
-        }, {
-            name: 'Replies',
-            data: [20, 7, 6, 5, 1, 2, 1, 2, 1, 1, 2, 2]
+            data: [130, 100, 52, 35, 26, {y:18, color: '#64b0ec'}, 10, 8, 4, 3, 3, 1]
+        }]
+    };
+    vm.data7 = {
+        chart: {type: 'bar', height: 300, backgroundColor: 'rgba(255, 255, 255, 0.8)'},
+        credits: {enabled: false},
+        title: {text: ''},
+        subtitle: {text: 'Who has the top 50 posts?'},
+        xAxis: {
+            lineColor: 'black',
+            categories: ['Porsche(North America)', 'Mazda USA', 'Mitsubishi Motors', 'Mercedes-Benz', 'Kia']
+        },
+        yAxis: {reversedStacks: false, visible: false, gridLineColor: 'transparent', min: 0, title: {text: ''}},
+        legend: {reversed: false},
+        plotOptions: {series: {stacking: 'normal'}},
+        colors: ['#6b7889'],
+        series: [{
+            showInLegend: false,
+            name: '',
+            data: [7, 6, 6, 4, 4]
+        }]
+    };
+    vm.data8 = {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie', height: 300, backgroundColor: 'rgba(255, 255, 255, 0.8)'
+        },
+        title: {
+            text: ''
+        },
+        subtitle: {
+            text: 'What type of posts were they?'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Video',
+                y: 100
+            }]
+        }]
+    };
+    vm.data9 = {
+        chart: {type: 'column', height: 300, backgroundColor: 'rgba(255, 255, 255, 0.8)'},
+        credits: {enabled: false},
+        title: {text: ''},
+        subtitle: {text: 'When were they posted?'},
+        xAxis: {
+            lineColor: 'black',
+            categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+        },
+        yAxis: {reversedStacks: false, visible: false, gridLineColor: 'transparent', min: 0, title: {text: ''}},
+        legend: {reversed: false},
+        colors: ['#6b7889'],
+        series: [{
+            showInLegend: false,
+            name: '',
+            data: [1, 6, 7, 10, 18, 8, 0]
         }]
     };
 });
