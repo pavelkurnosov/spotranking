c77f0a3c730098e64ffbad2a77f66505d2204dcb
pavelkurnosov <pavel.kurnosov2016@yandex.com>
2017-04-08 12:07:16 +0200

    done social

--- build/app.js
+++ build/app.js
@@ -15342,12 +15342,12 @@ angular.module('app.biz').controller('CrossChannelsSocialPostsController', funct
     vm.edate = moment().format('YYYY-MM-DD');
 
     vm.socialMedias = [
-        {id: 1, title: 'Instagram', icon:'instagram'},
-        {id: 2, title: 'Google+', icon:'google-plus'},
-        {id: 3, title: 'Facebook', icon:'facebook'},
-        {id: 4, title: 'Twitter', icon:'twitter'},
-        {id: 5, title: 'Youtube', icon:'youtube'},
-        {id: 6, title: 'Pinterest', icon:'pinterest'}
+        {id: 1, title: 'Instagram', icon: 'instagram'},
+        {id: 2, title: 'Google+', icon: 'google-plus'},
+        {id: 3, title: 'Facebook', icon: 'facebook'},
+        {id: 4, title: 'Twitter', icon: 'twitter'},
+        {id: 5, title: 'Youtube', icon: 'youtube'},
+        {id: 6, title: 'Pinterest', icon: 'pinterest'}
     ];
     vm.data1 = {
         chart: {
@@ -15396,55 +15396,107 @@ angular.module('app.biz').controller('CrossChannelsSocialPostsController', funct
         }]
     };
     vm.data3 = {
-        chart: {
-            type: 'spline'
-        },
+        chart: {type: 'spline'},
         credits: {enabled: false},
-        title: {
-            text: ''
-        },
-        subtitle: {
-            text: ''
-        },
-        xAxis: {
-            categories: ['12am', '-', '4am', '-', '8am', '-', '12pm', '-', '4pm', '-', '8pm', '-']
-        },
+        title: {text: ''},
+        subtitle: {text: ''},
+        xAxis: {categories: ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm']},
         yAxis: {
-            title: {
-                text: ''
-            },
-            labels: {
+            title: {text: ''}, labels: {
                 formatter: function () {
                     return this.value + '';
                 }
             }
         },
-        tooltip: {
-            crosshairs: true,
-            shared: true
-        },
-        plotOptions: {
-            spline: {
-                marker: {
-                    radius: 4,
-                    lineColor: '#666666',
-                    lineWidth: 1
-                }
-            }
-        },
+        tooltip: {crosshairs: true, shared: true},
+        plotOptions: {spline: {marker: {radius: 4, lineColor: '#666666', lineWidth: 1}}},
         series: [{
             name: 'Landscape Activity',
-            marker: {
-                symbol: 'square'
-            },
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            marker: {symbol: 'square'},
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0.05, 0.19, 0.38, 0.43, 0.43, 0.05, 0.05, 0.29, 0.67, 0.29, 0.38, 0.10, 0.05, 0.05, 0, 0]
 
         }, {
             name: 'Ooredoo Activity',
-            marker: {
-                symbol: 'diamond'
-            },
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            marker: {symbol: 'diamond'},
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.43, 0, 0.14, 0, 0, 0.71, 0, 0, 0.43, 0.14, 0, 0, 0, 0]
+        }]
+    };
+    vm.data4 = {
+        chart: {type: 'spline'},
+        credits: {enabled: false},
+        title: {text: ''},
+        subtitle: {text: ''},
+        xAxis: {categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']},
+        yAxis: {
+            title: {text: ''}, labels: {
+                formatter: function () {
+                    return this.value + '';
+                }
+            }
+        },
+        tooltip: {crosshairs: true, shared: true},
+        plotOptions: {spline: {marker: {radius: 4, lineColor: '#666666', lineWidth: 1}}},
+        series: [{
+            name: 'Landscape Activity',
+            marker: {symbol: 'square'},
+            data: [2.67, 3, 4.67, 3.67, 4.67, 2.33, 0]
+
+        }, {
+            name: 'Ooredoo Activity',
+            marker: {symbol: 'diamond'},
+            data: [1.5, 1, 2, 2, 3, 2, 0]
+        }]
+    };
+    vm.data5 = {
+        chart: {type: 'spline'},
+        credits: {enabled: false},
+        title: {text: ''},
+        subtitle: {text: ''},
+        xAxis: {categories: ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm']},
+        yAxis: {
+            title: {text: ''}, labels: {
+                formatter: function () {
+                    return this.value + '%';
+                }
+            }
+        },
+        tooltip: {crosshairs: true, shared: true},
+        plotOptions: {spline: {marker: {radius: 4, lineColor: '#666666', lineWidth: 1}}},
+        series: [{
+            name: 'Landscape Activity',
+            marker: {symbol: 'square'},
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0.004, 0.036, 0.053, 0.035, 0, 0.015, 0.031, 0.026, 0.058, 0.038, 0.21, 0.002, 0.008, 0, 0, 0]
+
+        }, {
+            name: 'Ooredoo Activity',
+            marker: {symbol: 'diamond'},
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.022, 0, 0.32, 0, 0, 0.34, 0, 0, 0.045, 0.37, 0, 0, 0, 0]
+        }]
+    };
+    vm.data6 = {
+        chart: {type: 'spline'},
+        credits: {enabled: false},
+        title: {text: ''},
+        subtitle: {text: ''},
+        xAxis: {categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']},
+        yAxis: {
+            title: {text: ''}, labels: {
+                formatter: function () {
+                    return this.value + '%';
+                }
+            }
+        },
+        tooltip: {crosshairs: true, shared: true},
+        plotOptions: {spline: {marker: {radius: 4, lineColor: '#666666', lineWidth: 1}}},
+        series: [{
+            name: 'Landscape Activity',
+            marker: {symbol: 'square'},
+            data: [0.030,0.026,0.040,0.048,0.092,0.033,0]
+
+        }, {
+            name: 'Ooredoo Activity',
+            marker: {symbol: 'diamond'},
+            data: [0.035,0.003,0.048,0.051,0.14,0.020,0]
         }]
     };
 
