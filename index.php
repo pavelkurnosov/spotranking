8a9ec52d3f4bdb90edc4ca0d759bcb3b93f5feb9
pavelkurnosov <pavel.kurnosov2016@yandex.com>
2017-04-25 14:31:20 +0200

    changed bg

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-Type: application/json");
+header("Content-Type: text/html");
 header("Expires: 0");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
