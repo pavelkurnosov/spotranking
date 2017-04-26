f2ea70a9587d3d4e964f9952405fa95f8fca29f1
pavelkurnosov <pavel.kurnosov2016@yandex.com>
2017-04-26 20:48:07 +0200

    plan price

--- app/biz/controllers/settings/PlanPriceCtrl.js
+++ app/biz/controllers/settings/PlanPriceCtrl.js
@@ -4,18 +4,24 @@ angular.module('app.biz').controller('PlanPriceController', function ($state) {
     var vm = this;
     vm.plan_type = 0;
     vm.prices = [{
-        monitoring_input: [12, 14, 16],
-        content_detection: [12, 14, 16],
-        digital_media: [11.4, 12.6, 15.9],
-        monitoring_ooh: [5, 7, 10],
-        measuring_output: [10.5, 14, 15],
-        other_service: [12, 14, 16]
+        monitoring_input: [12, 14],
+        content_detection: [12, 14],
+        digital_media: [11.4, 12.6],
+        monitoring_ooh: [5, 7],
+        measuring_output: [10.5, 14],
+        other_service: [12, 14]
     }, {
-        monitoring_input: [121, 142, 136],
-        content_detection: [112, 124, 163],
-        digital_media: [111.4, 122.6, 135.9],
-        monitoring_ooh: [51, 72, 103],
-        measuring_output: [110.5, 124, 135],
-        other_service: [121, 142, 163]
+        monitoring_input: [121, 142],
+        content_detection: [112, 124],
+        digital_media: [111.4, 122.6],
+        monitoring_ooh: [51, 72],
+        measuring_output: [110.5, 124],
+        other_service: [121, 142]
     }];
+    vm.sums = [0, 0];
+    for (var p in vm.prices[vm.plan_type]) {
+        vm.sums[0] += vm.prices[vm.plan_type][p][0] * 1;
+        vm.sums[1] += vm.prices[vm.plan_type][p][1] * 1;
+    }
+
 });
