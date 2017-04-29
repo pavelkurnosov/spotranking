92a8adacf88841401c7c2bed11477a04bef3199f
pavelkurnosov <pavel.kurnosov2016@yandex.com>
2017-04-29 05:17:45 +0200

    inventory map

--- app/biz/controllers/InventoryMap.js
+++ app/biz/controllers/InventoryMap.js
@@ -2,7 +2,22 @@
 
 angular.module('app.biz').controller('InventoryMapController', function () {
     var vm = this;
+    vm.countries = [
+        {id: "UA", name: "United Arab Eliminate"},
+        {id: "GE", name: "Germany"},
+        {id: "TK", name: "Turkey"},
+        {id: "FR", name: "France"},
+        {id: "IN", name: "India"}
+    ];
+    vm.currCountry = vm.countries[0];
 
+    vm.cities = [
+        {id: 1, name: 'Los Angels'},
+        {id: 2, name: 'London'},
+        {id: 3, name: 'Berlin'},
+        {id: 4, name: 'Amsterdam'}
+    ];
+    vm.currCity = vm.cities[0];
 
     vm.map = {
         center: {
